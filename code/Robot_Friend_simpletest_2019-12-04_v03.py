1af6a64812dcc29eca1f8985f77060e990e44f3e
CedarGroveStudios <jan@goolsbey.net>
2019-12-04 21:22:48 -0800

    update test code

--- code/Robot_Friend_simpletest_2019-12-04_v03.py
+++ code/Robot_Friend_simpletest_2019-12-04_v03.py
@@ -1,18 +1,15 @@
-# Robot_Friend_simpletest_2019-12-04_v02.py
-# uses revised adafruit_pybadger
+# Robot_Friend_simpletest_2019-12-04_v03.py
+# uses revised adafruit_pybadger and adafruit_crickit
 
 import time
 import random as rand
 
 # establish Crickit instance
 from adafruit_crickit import crickit
-crickit.init_neopixel(0)
-ss = crickit.seesaw
+crickit.init_neopixel(0, brightness=0.01)
+crickit.init_onboard_pixel(brightness=0.01)
 
-# set up on-board Seesaw NeoPixel
-from adafruit_seesaw.neopixel import NeoPixel
-sspixel = NeoPixel(ss, 27, 1, brightness=0.01)
-sspixel.fill([128, 0, 0])
+crickit.onboard_pixel[0] = ((255, 24, 255))
 
 # establish PyBadger instance
 from adafruit_pybadger import PyBadger
@@ -31,7 +28,7 @@ crickit.dc_motor_2.throttle = 0.0
 
 t1 = time.time()
 
-print("Robot_Friend_simpletest_2019-12-04_v02.py")
+print("Robot_Friend_simpletest_2019-12-04_v03.py")
 print("-----------------------------------------")
 
 while True:
@@ -44,8 +41,8 @@ while True:
     print((pybadger.light/35536, pybadger.acceleration[0] / 10, pybadger.acceleration[1] / 10, pybadger.acceleration[2] / 10))
 
     pybadger.pixels[int(t2) % 5] = [rand.randint(0, 4), rand.randint(0, 4), rand.randint(0, 4)]
-    sspixel[0] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
+    crickit.onboard_pixel[0] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
 
     pybadger.auto_dim_display(delay=10, movement_threshold=1)
 
-    time.sleep(0.5)
+    time.sleep(0.25)
