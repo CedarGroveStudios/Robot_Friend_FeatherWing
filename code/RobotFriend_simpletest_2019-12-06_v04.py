46305ee4699bd042955cd5b26ebe3ea060d29bd7
CedarGroveStudios <jan@goolsbey.net>
2019-12-06 16:17:26 -0800

    update simpletest

--- code/RobotFriend_simpletest_2019-12-06_v04.py
+++ code/RobotFriend_simpletest_2019-12-06_v04.py
@@ -1,4 +1,4 @@
-# Robot_Friend_simpletest_2019-12-04_v03.py
+# RobotFriend_simpletest_2019-12-06_v04.py
 # uses revised adafruit_pybadger and adafruit_crickit
 
 import time
@@ -7,9 +7,12 @@ import random as rand
 # establish Crickit instance
 from adafruit_crickit import crickit
 crickit.init_neopixel(0, brightness=0.01)
-crickit.init_onboard_pixel(brightness=0.01)
+crickit.onboard_pixel.brightness = 0.01
 
-crickit.onboard_pixel[0] = ((255, 24, 255))
+# set Crickit on-board NeoPixel to purple
+crickit.onboard_pixel[0] = (255, 24, 255)
+# or
+crickit.onboard_pixel.fill((255, 24, 255))
 
 # establish PyBadger instance
 from adafruit_pybadger import PyBadger
@@ -19,19 +22,18 @@ pybadger = PyBadger(pixels_brightness=0.01)
 pybadger.pixels.fill((255, 24, 255))  # dim red neopixels
 pybadger.brightness = 0.9  # set initial display brightness
 pybadger.show_badge(name_string="RobotFriend", hello_scale=2, my_name_is_scale=1, name_scale=2)
-time.sleep(3)
+time.sleep(2)
 pybadger.show_terminal()
 
 # turn off motors
 crickit.dc_motor_1.throttle = 0.0
 crickit.dc_motor_2.throttle = 0.0
 
+print("RobotFriend_simpletest_2019-12-06_v04.py")
+print("----------------------------------------")
+
 t1 = time.time()
-
-print("Robot_Friend_simpletest_2019-12-04_v03.py")
-print("-----------------------------------------")
-
-select_state = False
+select_state = True
 
 while True:
     t2 = time.monotonic()
@@ -46,14 +48,17 @@ while True:
         select_state = not(select_state)
 
     pybadger.pixels.fill([0, 0, 0])
-    print(t2, pybadger.light, pybadger.acceleration)
     # print(pybadger.joystick)  # if PyGamer
-    print(pybadger.button)
-    print((pybadger.light/35536, pybadger.acceleration[0] / 10, pybadger.acceleration[1] / 10, pybadger.acceleration[2] / 10))
+    # print(pybadger.button)  # to show state of buttons
+    print("----------")
+    print("T:%6.0f, L:%1.1f" % (t2, pybadger.light/35536))
+    print("X:%2.1f, Y:%2.1f, Z:%2.1f" % (pybadger.acceleration[0], pybadger.acceleration[1], pybadger.acceleration[2]))
+    # for plotter
+    # print((pybadger.light/35536, pybadger.acceleration[0] / 10, pybadger.acceleration[1] / 10, pybadger.acceleration[2] / 10))
 
     pybadger.pixels[int(t2) % 5] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
     crickit.onboard_pixel[0] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
 
-    pybadger.auto_dim_display(delay=20, movement_threshold=1)
+    pybadger.auto_dim_display(delay=10, movement_threshold=1)
 
     time.sleep(0.25)
