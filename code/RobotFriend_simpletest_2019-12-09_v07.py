0f303b6c6a4e66e55d63da07c7c6dd26c7ac3191
CedarGroveStudios <jan@goolsbey.net>
2019-12-09 22:43:03 -0800

    update panel abstraction and test code

---
+++ code/RobotFriend_simpletest_2019-12-09_v07.py
@@ -0,0 +1,157 @@
+# RobotFriend_simpletest_2019-12-09_v07.py
+# uses revised adafruit_pybadger and adafruit_crickit
+# uses cedargrove_pypanel to abstract Crickit and PyBadge/PyGamer
+
+import time
+import random as rand
+
+import cedargrove_pypanel as pypanel
+# panel function alias list
+crickit    = pypanel.crickit
+panel      = pypanel.pybadger
+stemma_dac = pypanel.dac
+terminalio = pypanel.terminalio
+label      = pypanel.label
+displayio  = pypanel.displayio
+display    = pypanel.display
+turtle     = pypanel.turtle
+board      = pypanel.board
+
+test_delay = 0.25  # seconds between tests
+
+try:
+    stemma_dac.normalized_value = 0.5  # 1.65v output
+    print("12-bit Stemma DAC --connected--")
+except:
+    print("12-bit Stemma DAC not connected (MCP4725)")
+
+# set Crickit on-board NeoPixel to purple
+crickit.onboard_pixel[0] = (255, 24, 255)
+# or
+crickit.onboard_pixel.fill((255, 24, 255))
+
+# PyBadge test
+panel.pixels.fill((255, 24, 255))  # dim purple neopixels
+panel.brightness = 0.9  # set initial display brightness
+panel.show_badge(name_string="RobotFriend", hello_scale=2, my_name_is_scale=1, name_scale=2)
+time.sleep(test_delay)
+panel.show_terminal()
+
+# Text area test
+# Set text, font, and color
+text = "HELLO WORLD"
+font = terminalio.FONT
+# font = pp.bitmap_font.load_font("/Helvetica-Bold-16.bdf")
+color = 0x0000FF
+# Create the text label
+text_area = label.Label(font, text="HELLO WORLD", color=0x00FF00)
+# Set the location
+text_area.x = 50
+text_area.y = 60
+# Show it
+panel.display.show(text_area)
+time.sleep(test_delay)
+
+# Bitmap graphics test
+with open("/RobotFriend_tiny.bmp", "rb") as bitmap_file:
+    # Setup the file as the bitmap data source
+    bitmap = displayio.OnDiskBitmap(bitmap_file)
+    # Create a TileGrid to hold the bitmap
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=displayio.ColorConverter())
+    # Create a Group to hold the TileGrid
+    group = displayio.Group()
+    # Add the TileGrid to the Group
+    group.append(tile_grid)
+    # Add the Group to the Display
+    display.show(group)
+    display.wait_for_frame()
+    time.sleep(test_delay)
+
+# Turtle graphics test
+def hilbert2(step, rule, angle, depth, t):
+    if depth > 0:
+        a = lambda: hilbert2(step, "a", angle, depth - 1, t)
+        b = lambda: hilbert2(step, "b", angle, depth - 1, t)
+        left = lambda: t.left(angle)
+        right = lambda: t.right(angle)
+        forward = lambda: t.forward(step)
+        if rule == "a":
+            left(); b(); forward(); right(); a(); forward(); a(); right(); forward(); b(); left()
+        if rule == "b":
+            right(); a(); forward(); left(); b(); forward(); b(); left(); forward(); a(); right()
+
+turtle = turtle(board.DISPLAY)
+turtle.penup()
+
+turtle.goto(-45, -45)
+turtle.pendown()
+hilbert2(6, "a", 90, 4, turtle)
+
+time.sleep(test_delay)
+
+sound_t = True  # wave file to speaker test
+if sound_t:
+    panel.play_tone(440, 0.25)
+    time.sleep(0.25)
+    panel.play_file("rimshot.wav")
+    time.sleep(test_delay)
+
+# turn off motors
+crickit.dc_motor_1.throttle = 0.0
+crickit.dc_motor_2.throttle = 0.0
+
+# DC motor test
+vel = 1.0
+crickit.dc_motor_1.throttle = vel
+crickit.dc_motor_2.throttle = vel
+time.sleep(test_delay)
+crickit.dc_motor_1.throttle = -1 * vel
+crickit.dc_motor_2.throttle = -1 * vel
+time.sleep(test_delay)
+crickit.dc_motor_1.throttle = 0.0
+crickit.dc_motor_2.throttle = 0.0
+time.sleep(test_delay)
+
+# test of Servo 1
+crickit.servo_1.angle = 90
+time.sleep(test_delay)
+crickit.servo_1.angle = 0
+time.sleep(test_delay)
+crickit.servo_1.angle = 180
+time.sleep(test_delay)
+
+print("RobotFriend_simpletest_2019-12-09_v07.py")
+print("----------------------------------------")
+
+t1 = time.time()
+select_state = True
+panel.show_terminal()
+
+while True:
+    t2 = time.monotonic()
+
+    if panel.button.select:
+        if select_state:
+            panel.show_badge(name_string="RobotFriend", hello_scale=2, my_name_is_scale=1, name_scale=2)
+        else:
+            panel.show_terminal()
+        while panel.button.select:
+            time.sleep(0.1)  # allow button to release
+        select_state = not(select_state)
+
+    panel.pixels.fill([0, 0, 0])
+    # print(panel.joystick)  # if PyGamer
+    # print(panel.button)  # to show state of buttons
+    print("----------")
+    print("T:%6.0f, L:%1.2f" % (t2, panel.light/65536))
+    print("X:%2.1f, Y:%2.1f, Z:%2.1f" % (panel.acceleration[0], panel.acceleration[1], panel.acceleration[2]))
+    # scale and format for plotter
+    print((int(panel.light / 65536 * 100) / 100, int(panel.acceleration[0] * 10) / 100,
+           int(panel.acceleration[1] * 10) / 100, int(panel.acceleration[2] * 10) / 100))
+
+    panel.pixels[int(t2) % 5] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
+    crickit.onboard_pixel[0] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
+
+    panel.auto_dim_display(delay=20, movement_threshold=1)
+
+    time.sleep(test_delay)
---
+++ code/cedargrove_pypanel.py
@@ -0,0 +1,31 @@
+# cedargrove_pypanel.py
+# Cedar Grove Studios 2019-12-09 v00
+# uses revised adafruit_pybadger and adafruit_crickit
+
+import board
+i2c = board.I2C()  # establish i2C instance for Stemma devices
+
+# Stemma-connected devices section
+import adafruit_mcp4725  # example for Stemma-attached 12-bit DAC
+dac = adafruit_mcp4725.MCP4725(i2c)
+
+# establish Crickit instance
+from adafruit_crickit import crickit
+crickit.init_neopixel(1, brightness=0.01)
+crickit.onboard_pixel.brightness = 0.01
+
+# establish PyBadger instance
+from adafruit_pybadger import PyBadger
+pybadger = PyBadger(pixels_brightness=0.01)
+
+# establish terminalio text display instance
+import terminalio
+from adafruit_display_text import label
+display = board.DISPLAY
+
+# establish displayio instance
+import displayio
+display = board.DISPLAY
+
+# establish turtle graphics instance
+from adafruit_turtle import turtle
