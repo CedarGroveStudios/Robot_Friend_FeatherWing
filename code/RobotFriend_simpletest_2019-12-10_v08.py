5c2b81e243d0f8448bab3d2b7cb3b779c563f21d
CedarGroveStudios <jan@goolsbey.net>
2019-12-10 22:24:39 -0800

    update test and pypanel library

--- code/RobotFriend_simpletest_2019-12-10_v08.py
+++ code/RobotFriend_simpletest_2019-12-10_v08.py
@@ -6,10 +6,17 @@ import time
 import random as rand
 from cedargrove_pypanel import *
 
+if hasattr(board, "JOYSTICK_X"):
+    print("Joystick found")  # if PyGamer
+    has_joystick = True
+else:
+    print("No joystick")
+    has_joystick = False
+
 test_delay = 0.25  # seconds between tests
 
 try:
-    stemma_dac.normalized_value = 0.5  # 1.65v output
+    stemma_dac.normalized_value = 0.0  # set DAC output to see if attached
     print("12-bit Stemma DAC --connected--")
 except:
     print("12-bit Stemma DAC not connected (MCP4725)")
@@ -38,7 +45,7 @@ text_area = label.Label(font, text="HELLO WORLD", color=0x00FF00)
 text_area.x = 50
 text_area.y = 60
 # Show it
-panel.display.show(text_area)
+display.show(text_area)
 time.sleep(test_delay)
 
 # Bitmap graphics test
@@ -112,6 +119,7 @@ print("----------------------------------------")
 
 t1 = time.time()
 select_state = True
+light_sensor = panel.light / 65536
 panel.show_terminal()
 
 while True:
@@ -126,18 +134,24 @@ while True:
             time.sleep(0.1)  # allow button to release
         select_state = not(select_state)
 
-    panel.pixels.fill([0, 0, 0])
-    # print(panel.joystick)  # if PyGamer
-    # print(panel.button)  # to show state of buttons
     print("----------")
-    print("T:%6.0f, L:%1.2f" % (t2, panel.light/65536))
-    print("X:%2.1f, Y:%2.1f, Z:%2.1f" % (panel.acceleration[0], panel.acceleration[1], panel.acceleration[2]))
-    # scale and format for plotter
-    print((int(panel.light / 65536 * 100) / 100, int(panel.acceleration[0] * 10) / 100,
-           int(panel.acceleration[1] * 10) / 100, int(panel.acceleration[2] * 10) / 100))
+    panel.pixels.fill([0, 0, 0])
+    if has_joystick:
+        print("Joystick:", panel.joystick)  # if PyGamer
 
-    panel.pixels[int(t2) % 5] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
-    crickit.onboard_pixel[0] = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
+    # print(panel.button)  # to show state of buttons
+
+    light_sensor = ((panel.light / 65536) + light_sensor) /2
+    x, y, z = panel.acceleration
+    print("Time:%6.0f, Light:%1.2f" % (t2, light_sensor))
+    print("X:%2.1f, Y:%2.1f, Z:%2.1f" % (x, y, z))
+    # scale and format for plotter
+    print((int(light_sensor * 100) / 100, int(x * 10) / 100,
+           int(y * 10) / 100, int(z * 10) / 100))
+
+    rgb = [rand.randint(64, 255), rand.randint(64, 255), rand.randint(64, 255)]
+    panel.pixels[int(t2) % 5] = rgb
+    crickit.onboard_pixel[0]  = rgb
 
     panel.auto_dim_display(delay=20, movement_threshold=1)
 
