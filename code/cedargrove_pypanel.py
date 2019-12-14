25eaf47ed6d3a54cfbae69adedbf4c0857f181d2
CedarGroveStudios <jan@goolsbey.net>
2019-12-13 22:04:37 -0800

    develop auto-config scheme

--- code/cedargrove_pypanel.py
+++ code/cedargrove_pypanel.py
@@ -5,18 +5,18 @@
 import board
 i2c = board.I2C()  # establish i2C instance for Stemma devices
 
-# Enumerate Stemma-connected devices
+# Enumerate Stemma/I2C-connected devices
 stemma = []  # clear the list
 
 import adafruit_mcp4725  # Stemma-attached 12-bit DAC
-try:  # test to confirm the module is attached to Stemma
+try:  # test to confirm the module is attached to Stemma/I2C
     mcp4725 = adafruit_mcp4725.MCP4725(i2c)
     mcp4725.normalized_value = 0.0  # set DAC output to see if attached
     stemma.append(("mcp4725", mcp4725, "12-bit DAC"))
 except: pass
 
 import adafruit_ssd1306  # Stemma-attached oled
-try:  # test to confirm the module is attached to Stemma
+try:  # test to confirm the module is attached to Stemma/I2C
     ssd1306 = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
     ssd1306.fill(0)
     stemma.append(("ssd1306", ssd1306, "128x32 OLED"))
@@ -24,8 +24,11 @@ except: pass
 
 # establish Crickit instance
 from adafruit_crickit import crickit
-crickit.init_neopixel(1, brightness=0.01)
-crickit.onboard_pixel.brightness = 0.01
+try:  # test to confirm the module is attached to Stemma/i2C
+    crickit.init_neopixel(1, brightness=0.01)
+    crickit.onboard_pixel.brightness = 0.01
+    stemma.append(("crickit", crickit, "Crickit Wing"))
+except: pass
 
 # establish PyBadger instance
 from adafruit_pybadger import PyBadger
---
+++ code/cedargrove_pypanel_config.py
@@ -0,0 +1,35 @@
+config = {
+	'mcp4725' : {
+		'name'     : 'MCP4725',
+		'desc'     : '12-bit DAC',
+		'import'   : 'import adafruit_mcp4725',
+		'instance' : 'mcp4725 = adafruit_mcp4725.MCP4725(i2c)',
+		'test'     : 'mcp4725.normalized_value = 0.0'
+		},
+
+	'ssd1306' : {
+		'name'     : 'SSD1306',
+		'desc'     : '128x 32 OLED',
+		'import'   : 'import adafruit_ssd1306',
+		'instance' : 'ssd1306 = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)',
+		'test'     : 'ssd1306.fill(0)'
+		},
+
+	'crickit' : {
+		'name'     : 'Crickit',
+		'desc'     : 'Crickit Wing',
+		'import'   : 'from adafruit_crickit import crickit',
+		'instance' : 'crickit.init_neopixel(1, brightness=0.01)',
+		'test'     : 'crickit.onboard_pixel.brightness = 0.01'
+		}
+}
+
+"""
+	class : {
+		'name'     : name,
+		'desc'     : description,
+		'import'   : import string to execute,
+		'instance' : instance string to execute,
+		'test'     : test string to execute
+		}
+"""
