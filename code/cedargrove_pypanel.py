c67ab51c6980d88ba14355671d0ef87b449b7061
CedarGroveStudios <jan@goolsbey.net>
2019-12-14 16:06:39 -0800

    update for semi-auto "plug-n-play" configurator

--- code/cedargrove_pypanel.py
+++ code/cedargrove_pypanel.py
@@ -1,6 +1,7 @@
 # cedargrove_pypanel.py
-# Cedar Grove Studios 2019-12-09-10 v00
+# Cedar Grove Studios 2019-12-14 v01
 # uses revised adafruit_pybadger and adafruit_crickit
+# configuration dictionary is contained in 'pypanel_config.py'
 
 import board
 i2c = board.I2C()  # establish i2C instance for Stemma devices
@@ -8,27 +9,23 @@ i2c = board.I2C()  # establish i2C instance for Stemma devices
 # Enumerate Stemma/I2C-connected devices
 stemma = []  # clear the list
 
-import adafruit_mcp4725  # Stemma-attached 12-bit DAC
-try:  # test to confirm the module is attached to Stemma/I2C
-    mcp4725 = adafruit_mcp4725.MCP4725(i2c)
-    mcp4725.normalized_value = 0.0  # set DAC output to see if attached
-    stemma.append(("mcp4725", mcp4725, "12-bit DAC"))
-except: pass
+try:
+    from pypanel_config import *
+except:
+    raise RuntimeError("Missing pypanel_config.py file")
 
-import adafruit_ssd1306  # Stemma-attached oled
-try:  # test to confirm the module is attached to Stemma/I2C
-    ssd1306 = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-    ssd1306.fill(0)
-    stemma.append(("ssd1306", ssd1306, "128x32 OLED"))
-except: pass
+st = []  # temporary list of possible devices
+for i in config.keys():
+    st.append(i)
 
-# establish Crickit instance
-from adafruit_crickit import crickit
-try:  # test to confirm the module is attached to Stemma/i2C
-    crickit.init_neopixel(1, brightness=0.01)
-    crickit.onboard_pixel.brightness = 0.01
-    stemma.append(("crickit", crickit, "Crickit Wing"))
-except: pass
+for j in range(0, len(st)):
+    try:
+        exec(config[st[j]]['import'])
+        exec(config[st[j]]['instance'])
+        exec(config[st[j]]['test'])
+        stemma.append((st[j], config[st[j]]['name'], config[st[j]]['desc']))
+    except:
+        pass
 
 # establish PyBadger instance
 from adafruit_pybadger import PyBadger
